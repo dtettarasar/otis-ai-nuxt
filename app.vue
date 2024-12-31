7ac224dbbc45bb019ce5c42fc51d48a51d5bb443
Dylan Tettarasar <dtettarasar@gmail.com>
2024-12-31 17:02:09 +0000

    created default layout

--- app.vue
+++ app.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
-    <!-- Exemple d’en-tête global -->
-    <header>
-      <h1>Mon Application Nuxt</h1>
-    </header>
 
     <!-- Contenu principal de la page -->
-    <main>
-      <NuxtPage />
-    </main>
-
-    <!-- Exemple de pied de page global -->
-    <footer>
-      <p>&copy; 2024 Mon Application</p>
-    </footer>
+    <div>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
 
     <!-- Annonceur de route pour l’accessibilité -->
     <NuxtRouteAnnouncer />
+    
   </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div>
+
+        <!-- Menu universel -->
+        <nav>
+            <ul>
+                <li><a href="/">Accueil</a></li>
+                <li><a href="/about">À propos</a></li>
+            </ul>
+        </nav>
+
+        <!-- Exemple d’en-tête global -->
+        <header>
+            <h1>Mon Application Nuxt</h1>
+        </header>
+
+        <main>
+            <slot></slot>
+        </main>
+
+        <!-- Exemple de pied de page global -->
+        <footer>
+            <p>&copy; 2024 Mon Application</p>
+        </footer>
+
+    </div>
+
+</template>
+  
