c4214d64cdbaf6babd455775fdc48f706fbd2b3b
Dylan Tettarasar <dtettarasar@gmail.com>
2025-01-04 12:10:37 +0000

    fixed bootstrap integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+
+  devtools: { enabled: true },
+
+  css: [
+    '~/assets/css/main.css' // Ajoute ce fichier au global
+  ],
+
+  plugins: [
+    { src: '~/plugins/bootstrap.js', mode: 'client' }
+  ],
 })
