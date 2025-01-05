1b1765bda8852d766ad20c2bfa66780dd045bff6
Dylan Tettarasar <dtettarasar@gmail.com>
2025-01-05 17:32:30 +0100

    created intro component

---
+++ components/generic/intro.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+
+    <div class="mt-5">
+        
+        <h2>Meet Otis!</h2>
+
+        <div>
+            <p>
+                Say goodbye to writer’s block and hello to effortless SEO-friendly articles! Otis AI is the ultimate tool for businesses and content creators who want to level up their content game without breaking a sweat.
+            </p>
+
+            <p>
+                All you need? A topic and some juicy keywords. That’s it. Otis takes care of the rest, whipping up polished, optimized articles faster than you can say "meta description." Once it’s ready, just grab your masterpiece and publish it on your site.
+            </p>
+
+            <p>
+                Whether you’re a seasoned marketer or a blogging newbie, Otis AI turns your ideas into click-worthy content with style, speed, and smarts. Ready to let Otis do the heavy lifting? Let’s get writing!
+            </p>
+        </div>
+
+    </div>
+
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,11 @@
 <template>
     <h2>This is the homepage component</h2>
     <generic-home-banner></generic-home-banner>
+
+    <div class="container">
+
+        <generic-intro></generic-intro>
+
+    </div>
+
 </template>
