7d5cb423415f41c31587617b7ed3a1399e04b305
Dylan Tettarasar <dtettarasar@gmail.com>
2025-01-07 18:00:04 +0100

    login page: build the title h2 in the page

--- pages/login.vue
+++ pages/login.vue
@@ -2,6 +2,15 @@
 
     <div>
         <generic-page-banner title="Let’s pick up where you left off." iconClass="bi-send-arrow-down-fill"></generic-page-banner>
+
+        <div class="container">
+
+            <div>
+                <h2>Access your dashboard</h2>
+            </div>
+
+        </div>
     </div>
+    
 
 </template>
