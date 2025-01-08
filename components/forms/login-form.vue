cfd47e482f04a0a937de96dc4b4800c94a705ec6
Dylan Tettarasar <dtettarasar@gmail.com>
2025-01-08 12:51:10 +0100

    building the login form

--- components/forms/login-form.vue
+++ components/forms/login-form.vue
@@ -1,38 +1,61 @@
+<script setup>
+
+import { ref, onMounted } from 'vue'
+
+// Reactive state
+const user = ref({ name: '', pwd: '' });
+const showError = ref(false);
+const showSuccess = ref(false);
+const showPassword = ref(false);
+const hideSubmitBtn = ref(false);
+
+const formEl = ref();
+
+// Methods 
+const togglePassword = () => {
+    showPassword.value = !showPassword.value;
+};
+
+const submitForm = () => {
+
+    console.log('init login process');
+    console.log('values');
+    console.log(user);
+
+
+}
+
+onMounted(() => {
+    console.log("login form is mounted");
+    console.log(formEl.value);
+})
+
+
+</script>
+
 <template>
 
     <div>
-        
-        <form method="post">
-
+        <form ref="formEl" @submit.prevent="submitForm" method="post">
             <p>Please use the form below to login to your account.</p>
 
             <div class="mb-3">
-
                 <label class="form-label" for="username"><i class="bi bi-person-fill"></i> <b>Username</b></label>
-                <input class="form-control" type="text" placeholder="Enter Username" name="username" id="username" required>
-
+                <input v-model="user.name" class="form-control" type="text" placeholder="Enter Username" name="username" id="username" required>
             </div>
 
             <div class="mb-3">
-
                 <label class="form-label" for="psw"><i class="bi bi-key-fill"></i> <b>Password</b></label>
-
                 <div class="input-group">
-
-                    <input class="form-control" type="password" placeholder="Enter Password" name="pwd" id="pwd" required>
-
-                    <button class="btn btn-outline-secondary" type="button">
-                        <i class="bi bi-eye-fill"></i>
-                        <!--<i class="bi bi-eye-slash-fill"></i>-->
+                    <input :type="showPassword ? 'text' : 'password'" v-model="user.pwd" class="form-control" placeholder="Enter Password" name="pwd" id="pwd" required>
+                    <button @click="togglePassword" class="btn btn-outline-secondary" type="button">
+                        <i v-if="!showPassword" class="bi bi-eye-fill"></i>
+                        <i v-else class="bi bi-eye-slash-fill"></i>
                     </button>
-
                 </div>
-                
             </div>
 
-            <button type="submit" class="btn btn-primary">Login</button>
-
-            <!--
+            <button v-if="!hideSubmitBtn" type="submit" class="btn btn-primary">Login</button>
 
             <div v-if="showError" class="alert mt-3 alert-danger" role="alert">
                 <i class="bi bi-exclamation-circle"></i> Authentication failed. Please check your credentials and try again.
@@ -41,11 +64,7 @@
             <div v-if="showSuccess" class="alert mt-3 alert-success" role="alert">
                 <i class="bi bi-check-circle"></i> You are now logged in. You will be redirected to your account page shortly.
             </div>
-
-            -->
-
         </form>
-
     </div>
 
 </template>
