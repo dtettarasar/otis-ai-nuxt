83115f980511414a739d08f9f221db4b4835cc7d
Dylan Tettarasar <dtettarasar@gmail.com>
2025-01-18 13:49:02 +0000

    build test for str encrypter module

--- tests/unit/backend/str_encrypter.test.js
+++ tests/unit/backend/str_encrypter.test.js
@@ -15,3 +15,18 @@ test('test the encrypter: make sure it returns an object', async () => {
     expect(testEncryption).toBeTypeOf('object');
 
 });
+
+test('test the object returned by the encrypter', async () => {
+
+    //console.log(testEncryption);
+
+    const ivRegex = /^[0-9a-f]{32}$/;
+    const encryptedStrRegex = /^[0-9a-f]+$/;
+
+    expect(testEncryption).toHaveProperty('iv');
+    expect(testEncryption).toHaveProperty('encryptedStr');
+
+    expect(testEncryption.iv).toMatch(ivRegex);
+    expect(testEncryption.encryptedStr).toMatch(encryptedStrRegex);
+
+});
