f808e67667a08050e8a83f899009007807d53704
Dylan Tettarasar <dtettarasar@gmail.com>
2025-01-31 16:56:38 +0000

    register page: add the register form in the template

--- pages/register.vue
+++ pages/register.vue
@@ -2,6 +2,18 @@
 
     <div>
         <generic-page-banner title="Your adventure starts here." iconClass="bi-person-fill-add"></generic-page-banner>
+
+        <div class="container">
+
+            <div>
+
+                <h2>Sign up for Otis</h2>
+                <forms-register-form></forms-register-form>
+
+            </div>
+
+        </div>
+
     </div>
     
 </template>
