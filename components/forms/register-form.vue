67581b4f110c6558504642b803cdc3893766bc0c
Dylan Tettarasar <dtettarasar@gmail.com>
2025-01-31 17:29:09 +0000

    register form : build the password field

--- components/forms/register-form.vue
+++ components/forms/register-form.vue
@@ -2,13 +2,42 @@
 
 import { ref, onMounted } from 'vue'
 
+// Reactive states
+
+const user = ref({
+
+    name: '', 
+    pwd: '',
+    pwdRepeat: '',
+    email:'',
+
+});
+
+const showError = ref(false);
+const showSuccess = ref(false);
+const showPassword = ref(false);
+const hideSubmitBtn = ref(false);
+const showPasswordRepeat = ref(false);
+
+const formEl = ref();
+
+// Methods 
+const togglePassword = () => {
+    showPassword.value = !showPassword.value;
+};
+
+onMounted(() => {
+    console.log("register form is mounted");
+    console.log(formEl.value);
+})
+
 </script>
 
 <template>
     
     <div>
 
-        <form>
+        <form ref="formEl">
 
             <p>Please use the form below to create your account.</p>
 
@@ -25,6 +54,25 @@ import { ref, onMounted } from 'vue'
                 <input class="form-control" type="email" placeholder="Enter Email" name="email" id="email" required>
 
             </div>
+
+            <div class="mb-3">
+
+                <label class="form-label" for="psw"><i class="bi bi-key-fill"></i> <b>Password</b></label>
+
+                <div class="input-group">
+
+                    <input :type="showPassword ? 'text' : 'password'" v-model="user.pwd" class="form-control" placeholder="Enter Password" name="pwd" id="pwd" required>
+
+                    <button @click="togglePassword" class="btn btn-outline-secondary" type="button">
+                        <i v-if="!showPassword" class="bi bi-eye-fill"></i>
+                        <i v-else class="bi bi-eye-slash-fill"></i>
+                    </button>
+
+                </div>
+
+            </div>
+
+            <button v-if="!hideSubmitBtn" type="submit" class="btn btn-primary">Create account</button>
 
         </form>
 
