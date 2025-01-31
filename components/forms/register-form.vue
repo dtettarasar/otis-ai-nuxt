43c523d5b1dda02125d699a3b516a365c06702d1
Dylan Tettarasar <dtettarasar@gmail.com>
2025-01-31 16:52:00 +0000

    created the form component file

---
+++ components/forms/register-form.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import { ref, onMounted } from 'vue'
+
+</script>
+
+<template>
+    
+    <div>
+
+        <form>
+
+            <p>Please use the form below to create your account.</p>
+
+        </form>
+
+    </div>
+
+</template>
