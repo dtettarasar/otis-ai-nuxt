61c8721f72268f52ede0ef288c5303e5257835bf
Dylan Tettarasar <dtettarasar@gmail.com>
2025-01-31 17:00:18 +0000

    register form: add the username and email field in the template

--- components/forms/register-form.vue
+++ components/forms/register-form.vue
@@ -12,6 +12,20 @@ import { ref, onMounted } from 'vue'
 
             <p>Please use the form below to create your account.</p>
 
+            <div class="mb-3">
+
+                <label class="form-label" for="username"><i class="bi bi-person-fill"></i> <b>Username</b></label>
+                <input class="form-control" type="text" placeholder="Enter Username" name="username" id="username" required>
+
+            </div>
+
+            <div class="mb-3">
+
+                <label class="form-label" for="email"><i class="bi bi-envelope-at-fill"></i> <b>Email</b></label>
+                <input class="form-control" type="email" placeholder="Enter Email" name="email" id="email" required>
+
+            </div>
+
         </form>
 
     </div>
